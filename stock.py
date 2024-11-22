38b3e176e8c78165fef9a8d8b98d34eb526d8732
Rohit Kumar <rohitgitpro@gmail.com>
2024-11-22 16:02:23 +0530

    this is a progra to solve gfg problem of stock market sell at maximum profit # given the data of days prize where buy at lowest and sell at highest # input will be given as the list

---
+++ stock.py
@@ -0,0 +1,33 @@
+#this is a progra to solve gfg problem of stock market sell at maximum profit 
+# given the data of days prize where buy at lowest and sell at highest 
+# input will be given as the list
+#function to check if the stock could be bought
+def stock(list):
+    buy =list[0]
+    for i in range(list):
+        if(list[i]>buy):
+            buy = list[i]
+        else:
+            i += 1
+    stocksell(buy)
+def stocksell(buy,list):
+    sell= list[0]
+    for i in range(list):
+        if(list[i]<sell):
+            sell = list[i]
+        else:
+            i += 1
+    if(buy>=sell):
+        print("can't buy this stalk")
+    else:
+        profit = sell - buy
+        print(f"profit is {profit}")
+def inputdata():
+    listdata = []
+    print("this is a program which will decide wether to buy a stock or not based on the price history ")
+    n = int(input("enter the number of data elements: "))
+    for i in range(0,n):
+        ele = int(input( ))
+        listdata.append(ele)
+    stock(listdata)
+inputdata()
